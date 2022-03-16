a1e4019cda8886d9a73982f08042275a57b2b9c0
rhinoxi <bellanchor@gmail.com>
2022-03-16 13:40:05 +0800

    update

--- lvim/config.lua
+++ lvim/config.lua
@@ -190,12 +190,15 @@ lvim.builtin.telescope.defaults.cache_picker = {
   num_pickers = 2,
 }
 
+lvim.builtin.dap.active = true
+
 -- replace whichkey mappings
 lvim.builtin.which_key.mappings = {
   ["/"] = { "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", "Comment" },
   ["c"] = { "<cmd>BufferKill<CR>", "Close Buffer" },
   ["f"] = { require("lvim.core.telescope.custom-finders").find_project_files, "Find File" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
+  ["e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
   b = {
     name = "Buffers",
     j = { "<cmd>BufferLinePick<cr>", "Jump" },
@@ -219,6 +222,22 @@ lvim.builtin.which_key.mappings = {
       "<cmd>BufferLineSortByExtension<cr>",
       "Sort by language",
     },
+  },
+  d = {
+    name = "Debug",
+    t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+    b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
+    c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+    C = { "<cmd>lua require'dap'.run_to_cursor()<cr>", "Run To Cursor" },
+    d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
+    g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
+    i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
+    o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
+    u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
+    p = { "<cmd>lua require'dap'.pause.toggle()<cr>", "Pause" },
+    r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
+    s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
+    q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
   },
   p = {
     name = "Packer",
--- lvim/ftplugin/go.lua
+++ lvim/ftplugin/go.lua
@@ -1,1 +1,5 @@
+local opts = {
+  cmd = { vim.fn.stdpath "data" .. "/lsp_servers/go/gopls"},
+}
+require("lvim.lsp.manager").setup("gopls", opts)
 vim.cmd("setlocal tabstop=4 shiftwidth=4")
